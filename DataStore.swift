88fcb4ae2062950b7f054be90b32cf6b0e94dcc9
Tim McEwan <isudochown@gmail.com>
2017-07-27 13:40:27 -0600

    Finally completed capstone

--- DataStore.swift
+++ DataStore.swift
@@ -7,3 +7,9 @@
 //
 
 import Foundation
+class DataStore {
+    
+    static let sharedInstnce = DataStore()
+    private init() {}
+    var memberAdd: [MemberAdd] = []
+}
--- MembersAdd.swift
+++ MembersAdd.swift
@@ -5,5 +5,44 @@
 //  Created by sudo on 7/3/17.
 //  Copyright © 2017 sudo. All rights reserved.
 //
-
 import Foundation
+//1 - Adopt the NSObject and NSCoding protocols
+class MemberAdd: NSObject, NSCoding {
+    
+    //5 - A safe way of naming my decoder key(s)
+    struct Keys {
+        static let name = "name"
+    }
+    
+    private var _name = ""
+    
+    override init() {}
+    
+    //4 - my own initializer
+    init(name: String) {
+        self._name = name
+    }
+    
+    //2 - this decodes our objects; this isn't called explicitly, it will be called with NSKeyedArchiver
+    required init(coder decoder: NSCoder) {
+        //this retrieves our saved name object and casts it as a string
+        if let nameObject = decoder.decodeObject(forKey: Keys.name) as? String {
+            _name = nameObject
+        }
+    }
+    
+    //3 - this encodes our objects (saves them)
+    func encode(with coder: NSCoder) {
+        //we are saving the name for the key "name"
+        coder.encode(_name, forKey: Keys.name)
+    }
+    
+    var name: String {
+        get {
+            return _name
+        }
+        set {
+            _name = newValue
+        }
+    }
+}
