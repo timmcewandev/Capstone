f6efd0b85ae9e0db2d01075fd8505f28b007a27d
Tim McEwan <blk_hotsauce@yahoo.com>
2018-09-12 13:33:18 -0600

    Added spinner on youtube tab

--- member YoutubeViewController.swift
+++ member YoutubeViewController.swift
@@ -9,14 +9,24 @@ import Fabric
 import Crashlytics
 
 class memberYoutubeViewController: UIViewController {
+  
   var member = Members()
+  let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .gray)
   @IBOutlet var webView: UIWebView!
   
   func loadYoutube(videoID:String) {
+    
     guard
       let youtubeURL = URL(string: videoID)
       else { return }
     webView.loadRequest( URLRequest(url: youtubeURL) )
+  }
+  override func viewWillAppear(_ animated: Bool) {
+    
+    view.addSubview(activityIndicator)
+    activityIndicator.frame = view.bounds
+    activityIndicator.startAnimating()
+    activityIndicator.removeFromSuperview()
   }
   override func viewDidLoad() {
     
@@ -24,10 +34,12 @@ class memberYoutubeViewController: UIViewController {
     var storage = self.member.youtube
     if (storage == "https://www.youtube.com/ /videos") {
       storage = "https://www.youtube.com/TeamKaliber"
+      
     }else {
     }
     loadYoutube(videoID: storage)
   }
+  
 }
 
 
